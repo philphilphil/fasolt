4c1651ee5b5523ff16f47a107015ef1cded17943
Phil Baum <phil@phib.io>
2026-03-22 08:55:26 +0000

    auto migrate

--- fasolt.Server/Program.cs
+++ fasolt.Server/Program.cs
@@ -105,6 +105,12 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.MigrateAsync();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
